af6380fc2049c2586fc62d8949f48209a4d866ca
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2020-10-21 19:11:32 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,22 @@
+class paper{
+    constructor(x,y,r){
+        var options = {
+            isStatic:false,
+            restitution:0.8,
+            friction:0.5,
+            density:1.2
+        }
+        this.r=r
+        this.body = Bodies.circle(x,y,this.r,options)
+        this.image = loadImage("paper.png")
+        World.add(world,this.body)
+
+    }
+    display(){
+        var ppos = this.body.position
+        imageMode(CENTER)
+        image(this.image,0,0,this.r,this)
+        ellipseMode(RADIUS)
+        ellipse(ppos.x,ppos.y,this.r,this.r)
+    }
+}
